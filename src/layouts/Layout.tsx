46c30f13490d19c25bd015b1cda4c51152305ca7
samueldsr8 <samueldsr8@gmail.com>
2022-02-06 15:48:08 -0500

    feat: Add main `Layout`

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import Head from 'next/head'
+import Navbar from '../components/Navbar'
+
+export type LayoutProps = {}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Popdynio</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <div className="p-10">{children}</div>
+      </main>
+    </>
+  )
+}
+
+export default Layout
