d3a0edb2a31be3c0296c11e5762ee5de53aae83f
samueldsr8 <samueldsr8@gmail.com>
2022-02-11 00:40:39 -0500

    feat: Add bullet to Popdynio title

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -8,7 +8,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
       <Head>
-        <title>Popdynio</title>
+        <title>&#9899; Popdynio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header>
